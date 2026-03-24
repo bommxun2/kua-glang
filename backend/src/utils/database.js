3dc5b1bc670d08ec0b3445443abf78c00fc4bbfd
Bommxun2 <wachirakorn.cn@gmail.com>
2026-03-24 16:44:10 +0700

    change database config

--- backend/src/utils/database.js
+++ backend/src/utils/database.js
@@ -1,10 +1,15 @@
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 const { DynamoDBDocumentClient } = require("@aws-sdk/lib-dynamodb");
 
-const dynamoDbClient = new DynamoDBClient({
+const clientConfig = {
   region: "us-east-1",
-});
+};
 
+if (process.env.DYNAMODB_ENDPOINT) {
+  clientConfig.endpoint = process.env.DYNAMODB_ENDPOINT;
+}
+
+const dynamoDbClient = new DynamoDBClient(clientConfig);
 const docClient = DynamoDBDocumentClient.from(dynamoDbClient);
 
 module.exports = docClient;

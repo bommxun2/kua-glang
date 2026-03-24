9217035a69be575532968f7b6d683d42c2b87e7f
Bommxun2 <wachirakorn.cn@gmail.com>
2026-03-24 16:48:03 +0700

    create vpc public and private

---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,32 @@
+variable "project_name" {
+  description = "Project name"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment (e.g., prod, dev)"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  description = "List of private subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.11.0/24", "10.0.12.0/24"]
+}
+
+variable "azs" {
+  description = "Availability Zones"
+  type        = list(string)
+}
